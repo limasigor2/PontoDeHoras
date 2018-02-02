8d4d11f463c7bb4f1820c4557ad647879f7528fa
Igor Lima <igorlima_es@alu.ufc.br>
2018-02-02 11:30:55 -0300

    Removendo o modelo User e tudo o que está relacionado a ele(service, repositório,...)

--- src/main/java/com/pdh/security/SecurityConfig.java
+++ src/main/java/com/pdh/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-				.usersByUsernameQuery("select username,password from users where username=?")
+				.usersByUsernameQuery("select username,password, enabled from users where username=?")
 				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
  
