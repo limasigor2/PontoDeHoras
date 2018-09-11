fc1cf77129cb216560911ca9886aa61fc81b6af9
Igor Lima <limasigor2@gmail.com>
2018-09-11 16:26:03 -0300

    Resolvendo problemas com o tomcat

--- src/main/java/com/pdh/PontoDeHorasApplication.java
+++ src/main/java/com/pdh/PontoDeHorasApplication.java
@@ -2,10 +2,18 @@ package com.pdh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PontoDeHorasApplication {
+public class PontoDeHorasApplication extends SpringBootServletInitializer{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PontoDeHorasApplication.class, args);
 	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PontoDeHorasApplication.class);
+	}
+
 }
